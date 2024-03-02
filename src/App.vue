85393dc6d5d57cccca3d39f29e0d50685960e9e2
Tsogzolmaa Munkhbat <tsogzolmaamonkhbat@gmail.com>
2024-03-02 20:41:18 +0100

    use example

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,76 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <div>
-      Sain uu?
+      Public Transportation Infographic
     </div>
+
+    <div class="hamgiin-gadna-taliin-hairtsag">
+      <div class="dotorh-hairtsag-1">
+        <div class="jijig-hairtsag-1">
+          1
+        </div>
+        <div class="jijig-hairtsag-2">
+          2
+        </div>
+      </div>
+      <div class="dotorh-hairtsag-2">
+        <div class="jijig-hairtsag-3">
+          3
+        </div>
+        <div class="jijig-hairtsag-4">
+          4
+        </div>
+      </div>
+    </div>
+
+
   </header>
 
   <RouterView />
 </template>
 
 <style scoped>
+.hamgiin-gadna-taliin-hairtsag {
+  border: 1px solid black;
+  height: 500px;
+}
+.dotorh-hairtsag-1 {
+  border: 1px solid black;
+  height: 30px;
+
+  display: flex;
+}
+.dotorh-hairtsag-2 {
+  border: 1px solid black;
+
+  height: calc(500px - 30px);
+  display: flex;
+}
+
+
+
+.jijig-hairtsag-1 {
+  background: green;
+  /* border: 1px solid; */
+  width: 30%;
+}
+
+.jijig-hairtsag-2 {
+  background: greenyellow;
+  /* border: 1px solid; */
+  width: 70%;
+}
+
+.jijig-hairtsag-3 {
+  background: white;
+  border: 1px solid;
+  width: 30%;
+}
+
+.jijig-hairtsag-4 {
+  background: white;
+  border: 1px solid;
+  width: 70%;
+}
 
 </style>
